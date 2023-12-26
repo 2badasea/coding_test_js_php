b3d89e071be73dc7440182a84ae8e4e5bdf0f2fa
이바다 <bada@hubsol.co.kr>
2023-12-26 20:32:11 +0900

    배열의 평균값 구하기

--- programmers/calculate_average_of_array.php
+++ programmers/calculate_average_of_array.php
@@ -1,7 +1,13 @@
 <?php
-    // php
+// php로 배열의 평균값 구하기
+$arr = [1,2,3,4,5,6,7,8,9,10];
 
+// array_reduce() 활용
+$answer = array_reduce($arr, function($pre, $cur){
+    return $pre + $cur;
+}, 0);
 
+echo $answer/count($arr);
 
 
 
@@ -11,17 +17,35 @@
     // 제이쿼리
     $(function() {
 
+        let answer2 = 0;
+
+        let arr2 = [1, 2, 3, 4, 5];
+
+        $.each(arr2, function(index, item) {
+            answer2 += item;
+        })
+
+        console.log('제이쿼리 답');
+        console.log(answer2 / arr2.length);
 
 
 
     })
 </script>
 <script>
-    // 자바스크립트
+    // 자바스크립트. 배열의 평균값 구하기
+    const arr = [1, 2, 3, 4];
 
+    // 1. reduce() 메서드 활용
+    function reduce_solution(arr) {
+        // 1. reduce() 메서드 활용    
+        let answer = arr.reduce((pre, cur) => {
+            return pre + cur;
+        }, 0);
 
-
-
+        // 자바스크립트 답
+        return answer / arr.length;
+    }
 </script>
 <div>
 
