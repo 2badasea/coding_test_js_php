b98662a4aa3bff76c2442b890dd4ddbc912e2a13
이바다 <bada@hubsol.co.kr>
2023-12-26 20:01:09 +0900

    커밋

---
+++ programmers/calculate_average_of_array.php
@@ -0,0 +1,28 @@
+<?php
+    // php
+
+
+
+
+
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>
+    // 제이쿼리
+    $(function() {
+
+
+
+
+    })
+</script>
+<script>
+    // 자바스크립트
+
+
+
+
+</script>
+<div>
+
+</div>
